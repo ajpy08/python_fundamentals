6a1b0e04c0948e9ff966306cb574fedcb148c04d
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2023-08-23 18:15:17 -0600

    feat: :sparkles: two new examples was added

---
+++ ejemplo_comparacion.py
@@ -0,0 +1,16 @@
+a = 5
+b = 10
+
+igual = a == b
+diferente = a != b
+mayor = a > b
+menor = a < b
+mayor_igual = a >= b
+menor_igual = a <= b
+
+print("Igual: ", igual)
+print("Diferente: ", diferente)
+print("Mayor: ", mayor)
+print("Menor: ", menor)
+print("Mayor igual: ", mayor_igual)
+print("Menor igual: ", menor_igual)
